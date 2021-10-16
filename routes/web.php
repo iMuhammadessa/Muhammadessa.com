94528e48390c122c1c16c4bd5cfbe2ce69cccd76
Muhammad Essa <imuhammadessa@gmail.com>
2021-10-16 10:19:57 +0500

    Modified changes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('pages.index');
+// });
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [
+    App\Http\Controllers\HomeController::class,
+    'index',
+])->name('home');
